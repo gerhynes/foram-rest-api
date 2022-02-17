8a777d773228ea6fd93fe190dd15bdafdfda7f67
Gerard Hynes <gkhynes@gmail.com>
2022-02-17 21:19:59 +0000

    Refactor to use id to track categories

--- src/main/scala/com/foram/Main.scala
+++ src/main/scala/com/foram/Main.scala
@@ -11,19 +11,18 @@ import akka.stream.ActorMaterializer
 import spray.json.DefaultJsonProtocol._
 import akka.util.Timeout
 import akka.pattern.ask
-import scala.collection.mutable.Map
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import spray.json._
 
-case class Category(name: String, description: String)
+case class Category(id: Int, name: String, description: String)
 
 object CategoryDB {
   case object GetAllCategories
-  case class GetCategory(name: String)
+  case class GetCategory(id: Int)
   case class AddCategory(category: Category)
-  case class UpdateCategory(name: String, category: Category)
+  case class UpdateCategory(id: Int, category: Category)
   case class RemoveCategory(category: Category)
   case object OperationSuccess
 }
@@ -31,36 +30,36 @@ object CategoryDB {
 class CategoryDB extends Actor with ActorLogging {
   import CategoryDB._
 
-  var categories: Map[String, Category] = Map()
+  var categories: Map[Int, Category] = Map()
 
   override def receive: Receive = {
     case GetAllCategories =>
       log.info(s"Searching for categories")
       sender() ! categories.values.toList
 
-    case GetCategory(name) =>
-      log.info(s"Finding category with name: $name")
-      sender() ! categories.get(name)
+    case GetCategory(id) =>
+      log.info(s"Finding category with name: $id")
+      sender() ! categories.get(id)
 
     case AddCategory(category) =>
       log.info(s"Adding category $category")
-      categories = categories + (category.name -> category)
+      categories = categories + (category.id -> category)
       sender() ! OperationSuccess
 
-    case UpdateCategory(name, category) =>
+    case UpdateCategory(id, category) =>
       log.info(s"Updating category $category")
-      categories = categories + (name -> category)
+      categories = categories + (id -> category)
       sender() ! OperationSuccess
 
     case RemoveCategory(category) =>
       log.info(s"Removing category $category")
-      categories = categories - category.name
+      categories = categories - category.id
       sender() ! OperationSuccess
   }
 }
 
 trait CategoryJsonProtocol extends DefaultJsonProtocol {
-  implicit val categoryFormat = jsonFormat2(Category)
+  implicit val categoryFormat = jsonFormat3(Category)
 }
 
 object Main extends App with CategoryJsonProtocol {
@@ -72,9 +71,9 @@ object Main extends App with CategoryJsonProtocol {
 
   val categoryDb = system.actorOf(Props[CategoryDB], "categoryDB")
   val categoryList = List(
-    Category("JavaScript", "Ask questions and share tips about JavaScript"),
-    Category("Java", "Ask questions and share tips about Java"),
-    Category("Scala", "Ask questions and share tips about Scala")
+    Category(1, "JavaScript", "Ask questions and share tips about JavaScript"),
+    Category(2, "Java", "Ask questions and share tips about Java"),
+    Category(3, "Scala", "Ask questions and share tips about Scala")
   )
 
   categoryList.foreach { category =>
@@ -86,8 +85,8 @@ object Main extends App with CategoryJsonProtocol {
   val categoriesRoutes =
     pathPrefix("api" / "categories") {
       get {
-        (path(Segment) | parameter('name)) { name =>
-          val categoryOptionFuture: Future[Option[Category]] = (categoryDb ? GetCategory(name)).mapTo[Option[Category]]
+        (path(IntNumber) | parameter('id.as[Int])) { id =>
+          val categoryOptionFuture: Future[Option[Category]] = (categoryDb ? GetCategory(id)).mapTo[Option[Category]]
           complete(categoryOptionFuture)
         } ~
           pathEndOrSingleSlash {
@@ -100,9 +99,9 @@ object Main extends App with CategoryJsonProtocol {
           }
         } ~
         put {
-          (path(Segment) | parameter('name)) { name =>
+          (path(IntNumber) | parameter('id.as[Int])) { id =>
             entity(as[Category]) { category =>
-              complete((categoryDb ? UpdateCategory(name, category)).map(_ => StatusCodes.OK))
+              complete((categoryDb ? UpdateCategory(id, category)).map(_ => StatusCodes.OK))
             }
           }
         } ~
