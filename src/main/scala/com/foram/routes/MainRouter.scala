a644dd8d66318283145335ecbfc75bdc2fadd705
gerhynes <gkhynes@gmail.com>
2022-03-16 10:25:32 +0000

    Reimplement rejection and exception handling

--- src/main/scala/com/foram/routes/MainRouter.scala
+++ src/main/scala/com/foram/routes/MainRouter.scala
@@ -1,18 +1,37 @@
 package com.foram.routes
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{Directive, ExceptionHandler, RejectionHandler, Route}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 
 object MainRouter {
 
-  val categoryRoutes = CategoryRoutes.routes
-  val userRoutes = UserRoutes.routes
-  val topicRoutes = TopicRoutes.routes
-  val postRoutes = PostRoutes.routes
+  val categoryRoutes: Route = CategoryRoutes.routes
+  val userRoutes: Route = UserRoutes.routes
+  val topicRoutes: Route = TopicRoutes.routes
+  val postRoutes: Route = PostRoutes.routes
 
-//  val routes = CategoryRoutes.routes ~ UserRoutes.routes ~ TopicRoutes.routes ~ PostRoutes.routes
+  val rejectionHandler: RejectionHandler = corsRejectionHandler.withFallback(RejectionHandler.default)
 
-  val routes = cors() {
-    concat(categoryRoutes, userRoutes, topicRoutes, postRoutes)
+  val exceptionHandler: ExceptionHandler = ExceptionHandler {
+    case e: NoSuchElementException =>
+      complete(StatusCodes.NotFound, "Cannot find resource")
+    case e: ClassCastException =>
+      complete(StatusCodes.NotFound, "Incorrect resource returned")
+    case e: RuntimeException =>
+      complete(StatusCodes.NotFound, e.getMessage)
+    case e: IllegalArgumentException =>
+      complete(StatusCodes.BadRequest, "Illegal argument passed")
+  }
+
+  val handleErrors: Directive[Unit] = handleRejections(rejectionHandler) & handleExceptions(exceptionHandler)
+
+  val routes: Route = {
+    cors() {
+      handleErrors {
+        concat(categoryRoutes, userRoutes, topicRoutes, postRoutes)
+      }
+    }
   }
 }
