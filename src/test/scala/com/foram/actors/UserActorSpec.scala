7a7a3dd953cfc81c173a5745f1f8e32fc13875a4
gerhynes <gkhynes@gmail.com>
2022-04-04 10:52:40 +0100

    Add unit tests for UserActor

--- src/test/scala/com/foram/actors/UserActorSpec.scala
+++ src/test/scala/com/foram/actors/UserActorSpec.scala
@@ -2,14 +2,13 @@ package com.foram.actors
 
 import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
-import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.Timeout
-import com.foram.actors.UserActor.GetAllUsers
+import com.foram.actors.UserActor.ActionPerformed
 import com.foram.dao.AbstractUsersDao
 import com.foram.models.User
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.BeforeAndAfterAll
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
@@ -30,18 +29,66 @@ class UserActorSpec extends TestKit(ActorSystem("MySpec"))
     TestKit.shutdownActorSystem(system)
   }
 
-  val user = User(UUID.fromString("33de6e57-c57c-4451-82b9-b73ae248c672"), "Quincy Lars", "quince", "qlars@example.com", OffsetDateTime.parse("2022-02-20T06:30:00.166Z"), OffsetDateTime.parse("2022-02-20T06:30:00.166Z"))
+  val sampleUser = User(UUID.fromString("33de6e57-c57c-4451-82b9-b73ae248c672"), "Quincy Lars", "quince", "qlars@example.com", OffsetDateTime.parse("2022-02-20T06:30:00.166Z"), OffsetDateTime.parse("2022-02-20T06:30:00.166Z"))
+  val mockUsersDao = stub[AbstractUsersDao]
+  val userActor = system.actorOf(Props(new UserActor(mockUsersDao)), "userActor")
 
   implicit val timeout = Timeout(5 seconds)
 
   "A UserActor" must {
-    "respond to getUsers with list of users" in {
-      val mockUsersDao = stub[AbstractUsersDao]
-      (mockUsersDao.findAll _).when().returns(Future(Seq(user)))
+    "respond to getAllUsers with a list of Users" in {
+      (mockUsersDao.findAll _).when().returns(Future(Seq(sampleUser)))
 
-      val userActor = system.actorOf(Props(new UserActor(mockUsersDao)), "userActor")
       val usersFuture = userActor ? UserActor.GetAllUsers
-      usersFuture map { users => assert(users === List[User](user)) }
+      usersFuture map { users => assert(users === List[User](sampleUser)) }
+    }
+    
+    "respond to getUserByID with a single User" in {
+      val uuid = UUID.fromString("33de6e57-c57c-4451-82b9-b73ae248c672")
+      (mockUsersDao.findById _).when(uuid).returns(Future(sampleUser))
+
+      val userFuture = userActor ? UserActor.GetUserByID(uuid)
+      userFuture map { user => assert(user === sampleUser) }
+    }
+
+    "respond to getUserByUsername with a single User" in {
+      val username = "Quincy Lars"
+      (mockUsersDao.findByUsername _).when(username).returns(Future(sampleUser))
+
+      val userFuture = userActor ? UserActor.GetUserByUsername(username)
+      userFuture map { user => assert(user === sampleUser) }
+    }
+
+    "respond to CreateUser with confirmation" in {
+      (mockUsersDao.create _).when(sampleUser).returns(Future(java.util.UUID.randomUUID))
+
+      val userFuture = userActor ? UserActor.CreateUser(sampleUser)
+      userFuture map { success =>
+        assert(success === ActionPerformed)
+        expectMsg("User created")
+      }
+    }
+
+    "respond to UpdateUser with confirmation" in {
+      val uuid = UUID.fromString("33de6e57-c57c-4451-82b9-b73ae248c672")
+      (mockUsersDao.update _).when(uuid, sampleUser).returns(Future(1))
+
+      val userFuture = userActor ? UserActor.UpdateUser(uuid, sampleUser)
+      userFuture map { success =>
+        assert(success === ActionPerformed)
+        expectMsg("User updated")
+      }
+    }
+
+    "respond to DeleteUser with confirmation" in {
+      val uuid = UUID.fromString("33de6e57-c57c-4451-82b9-b73ae248c672")
+      (mockUsersDao.delete _).when(uuid).returns(Future(1))
+
+      val userFuture = userActor ? UserActor.DeleteUser(uuid)
+      userFuture map { success =>
+        assert(success === ActionPerformed)
+        expectMsg("User deleted")
+      }
     }
   }
 }
